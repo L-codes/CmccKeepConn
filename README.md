3800a68711172885b1481a0bbcc281b4a693e6d6
L <314497322@qq.com>
2017-06-23 12:10:40 +0800

    push v1.0.0

---
+++ README.md
@@ -0,0 +1,33 @@
+# 0x00 CmccKeepConn
+cmccKeepConn 是一个可以在CMCC-FREE WIFI上自动认证的工具，区别于其它的自动认证脚本不管认证状态，只要启动脚本就不会断开认证
+
+项目地址：[https://github.com/L-codes/cmccKeepConn](https://github.com/L-codes/cmccKeepConn)
+
+# 0x01 Features
+- 支持python 2.x和3.x
+- 支持自动检测网络状态，无需启动和关闭脚本
+- 支持各地CMCC-FREE自动认证
+
+# 0x03 Use examples
+```
+  ____                    _  __                ____                  
+ / ___|_ __ ___   ___ ___| |/ /___  ___ _ __  / ___|___  _ __  _ __  
+| |   | '_ ` _ \ / __/ __| ' // _ \/ _ \ '_ \| |   / _ \| '_ \| '_ \ 
+| |___| | | | | | (_| (__| . \  __/  __/ |_) | |__| (_) | | | | | | |
+ \____|_| |_| |_|\___\___|_|\_\___|\___| .__/ \____\___/|_| |_|_| |_|
+                                       |_|                           
+               [ Author L       Version 1.0.0 ]
+
+[ Github ] https://github.com/L-codes/cmccKeepConn
+
+[+] auto keep connect ...
+[+] IP: 10.177.31.136, Phone: 13571410356
+
+[+] 11:35:59 - The 1st certification was successful
+[+] 12:25:59 - The 2nd certification was successful
+
+[Interrupt] I guess you're off duty
+```
+
+# 0x04 Problem
+如在使用过程中发现bug或有好的建议，欢迎提交[Issues](https://github.com/L-codes/cmccKeepConn/issues)和[Pull Requests](https://github.com/L-codes/cmccKeepConn/pulls)
---
+++ cmccKeepConn.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+#-*-coding:UTF-8-*-
+# Date 2017-06-05
+
+try:
+    import requests
+except:
+    raise SystemExit('[Error] Please install "requests": pip install requests')
+import time
+import re
+import random
+import signal
+
+__program__ = 'cmccKeepConn'
+__version__ = '1.0.0'
+__author__ = 'L'
+__github__ = 'https://github.com/L-codes/cmccKeepConn'
+
+def interrupt_handler(signalnum, frame):
+    raise SystemExit("\r    \n[Interrupt] I guess you're off duty")
+
+
+class CMCC_FREE:
+    def __init__(self, url):
+        print('[+] auto keep connect ...')
+        self.get_info(url)
+
+
+    def get_info(self, debug_url):
+        wlanacname, wlanuserip = self._get_url_info(debug_url)
+        phone = random.randint(13500000000, 13599999999)
+        url_head = 'http://221.176.66.85:81/wlan-portal-web/'
+        user_agent = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)'
+
+        self.url = url_head + 'service/portalAccountService.loginByPortal.rest?t=1459479089251'
+        self.headers = {
+            'Referer': url_head + 'portal?wlanacname=' + wlanacname + '&wlanuserip=' + wlanuserip + '&wlanacssid=CMCC-FREE&spe=ggmb',
+            'User-Agent': user_agent,
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Encoding': 'gzip, deflate',
+            'Pragma': 'no-cache',
+            'DNT': '1'
+        }
+
+        self.data = {
+            'paramMap.USER': phone,
+            'paramMap.PWD':  ' ' * 6,
+            'paramMap.wlanacname': wlanacname,
+            'paramMap.wlanuserip': wlanuserip,
+            'paramMap.actiontype': 'LOGIN',
+            'paramMap.wlanacssid': 'CMCC-FREE',
+            'paramMap.forceflag': 1,
+            'paramMap.useragent': user_agent,
+            'paramMap.spe': 'ggmb'
+        } 
+
+        print('[+] IP: {}, Phone: {}\n'.format(wlanuserip, phone))
+
+
+    def keep_conn(self, interval_min=50):
+        self.counter = 0
+        while True:
+            msg = self._post_request()
+            print(self._keep_status())
+            time.sleep(interval_min * 60)
+
+
+    def _keep_status(self):
+        self.counter += 1
+        t = time.strftime("%H:%M:%S", time.localtime())
+        top3 = {1: '1st', 2: '2nd', 3: '3rd'}
+        times = top3[self.counter] if self.counter in top3 else str(self.counter) + 'th'
+        return '[+] {} - The {} certification was successful'.format(t, times)
+        
+
+
+    def _post_request(self):
+        for _ in range(2):
+            r = requests.post(url=self.url, data=self.data, headers=self.headers)
+
+
+    def _get_url_info(self, url):
+        while True:
+            try:
+                r = requests.get(url=url, allow_redirects=True, timeout=3)
+            except requests.exceptions.ProxyError:
+                raise SystemExit('[Error] Can not connect to the network, please check the proxy settings')
+            except Exception:
+                pass
+            
+            wlanacname = re.search(r'wlanacname=(.*?)&', r.url)
+            wlanuserip = re.search(r'wlanuserip=(.*?)&', r.url)
+            if wlanacname and wlanuserip:
+                wlanacname = wlanacname.group(1)
+                wlanuserip = wlanuserip.group(1)
+                break
+            time.sleep(3)
+        return wlanacname, wlanuserip
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, interrupt_handler)
+    banner = '''
+  ____                    _  __                ____                  
+ / ___|_ __ ___   ___ ___| |/ /___  ___ _ __  / ___|___  _ __  _ __  
+| |   | '_ ` _ \ / __/ __| ' // _ \/ _ \ '_ \| |   / _ \| '_ \| '_ \ 
+| |___| | | | | | (_| (__| . \  __/  __/ |_) | |__| (_) | | | | | | |
+ \____|_| |_| |_|\___\___|_|\_\___|\___| .__/ \____\___/|_| |_|_| |_|
+                                       |_|                           
+               [ Author {}       Version {} ]
+
+[ Github ] {}
+'''.format(__author__, __version__, __github__)
+    print(banner)
+    cmcc = CMCC_FREE('http://github.com/L-codes')
+    cmcc.keep_conn()
+
